5dc2a1da689a74eea016295ceb61454aa5cc802f
Cristian <veracristian89@gmail.com>
2022-08-26 19:51:10 -0300

    inyeccion de dependencias en proyecto, resta realizar ngfor

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlus, faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { PortafolioService } from 'src/app/services/portafolio.service';
 
 @Component({
   selector: 'app-proyectos',
@@ -11,9 +12,14 @@ export class ProyectosComponent implements OnInit {
   faPen = faPen;
   faPlus = faPlus;
   titulo : string = "Proyectos";
-  constructor() { }
+  miPortafolio:any;
+  constructor(private datosPortafolio:PortafolioService ) { }
 
   ngOnInit(): void {
+    this.datosPortafolio.obtenerDatos().subscribe(data => {
+      console.log(data);
+      this.miPortafolio = data;
+    });
   }
 
 }
